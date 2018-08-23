352a9796d17a9a6eaa10a6860c425e9aa74b4183
gaobin <gaobin@bingao.local>
2018-08-23 12:18:58 +0800

    Initial Commit

---
+++ GameCenter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameCenter
+//
+//  Created by gaobin on 2018/8/23.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GameCenter/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GameCenter
+//
+//  Created by gaobin on 2018/8/23.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
