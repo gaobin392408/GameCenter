779b61f41b8bddf59f030a0e47b359a91e524d58
gaobin <gaobin08052009@126.com>
2018-08-23 17:45:08 +0800

    增加计算commentCell height高度 进行优化

---
+++ GameCenter/Games/Model/GBCommentFrame.h
@@ -0,0 +1,13 @@
+//
+//  GBCommentFrame.h
+//  GameCenter
+//
+//  Created by 高斌 on 2018/8/23.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBCommentFrame : NSObject
+
+@end
---
+++ GameCenter/Games/Model/GBCommentFrame.m
@@ -0,0 +1,13 @@
+//
+//  GBCommentFrame.m
+//  GameCenter
+//
+//  Created by 高斌 on 2018/8/23.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import "GBCommentFrame.h"
+
+@implementation GBCommentFrame
+
+@end
