1c87229474ea7d626e942a5703ce028fd498279b
gaobin <gaobin08052009@126.com>
2018-08-24 17:48:44 +0800

    网络请求 设置修改

---
+++ GameCenter/Games/ViewModel/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  GameCenter
+//
+//  Created by 高斌 on 2018/8/24.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Game : NSObject
+
+@end
---
+++ GameCenter/Games/ViewModel/Game.m
@@ -0,0 +1,13 @@
+//
+//  Game.m
+//  GameCenter
+//
+//  Created by 高斌 on 2018/8/24.
+//  Copyright © 2018年 bingao. All rights reserved.
+//
+
+#import "Game.h"
+
+@implementation Game
+
+@end
